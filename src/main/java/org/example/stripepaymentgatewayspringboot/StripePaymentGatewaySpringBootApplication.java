8b4d87cdff7642779ce4e20b16d17f2eb6c6c7bb
Ravi Kumar Gupta <babaravibbb@gmail.com>
2024-12-16 19:31:56 +0530

    first commit

---
+++ src/main/java/org/example/stripepaymentgatewayspringboot/StripePaymentGatewaySpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.stripepaymentgatewayspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripePaymentGatewaySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripePaymentGatewaySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stripepaymentgatewayspringboot/StripePaymentGatewaySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stripepaymentgatewayspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripePaymentGatewaySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
